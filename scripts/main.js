b775768918655de07bdf9cb42c672343abae9cfe
SinGala67 <87444864+TheanaProductions@users.noreply.github.com>
2021-08-31 16:17:03 +0200

    header logo instantiated through main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,15 +5,15 @@ add_logoBig();
 
 // Insantiate banner image
 function add_logoSmall() {
-    let logoSmall = document.createElement('logoSmall');
-    logoSmall.src = '../images/logoSmall.png';
+    let logoSmall = document.createElement('img');
+    logoSmall.src = 'images/logoSmall.png';
     logoSmall.id = 'img-small';
     document.getElementById('header').appendChild(logoSmall);
 }
 
 function add_logoBig() {
-    let logoBig = document.createElement('logoBig');
-    logoBig.src = '../images/logoBig.png';
+    let logoBig = document.createElement('img');
+    logoBig.src = 'images/logoBig.png';s
     logoBig.id = 'img-big';
     document.getElementById('header').appendChild(logoBig);
 }
